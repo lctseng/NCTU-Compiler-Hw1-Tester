deb395ca9b22d97f33bae914458cf753756a1818
lctseng <computerhenry321@yahoo.com.tw>
2014-10-19 15:49:28 +0800

    Add auto-run script

---
+++ core/auto-run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -ev
+
+: ==============================================
+:   Downloading from Github...
+: ==============================================
+
+git clone --depth=1 https://lctseng@github.com/lctseng/NCTU-Compiler-Hw1-Tester
+rm -rf /NCTU-Compiler-Hw1-Tester/.git
+
+: ==============================================
+:   Download finished!
+:   You can change into the 'NCTU-Compiler-Hw1-Tester' directory.
+:   To run test, just copy your xxx.l source file into 
+:   'NCTU-Compiler-Hw1-Tester' directory, then run './test xxx.l'
+: ==============================================
+
